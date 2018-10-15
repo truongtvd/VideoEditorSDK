856da15f9c89f50646b62d869aa660b98956e7e8
trungnk <macarongon@gmail.com>
2018-10-15 16:33:46 +0700

    create project

---
+++ VideoEditorSDK/VideoEditorSDK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoEditorSDK
+//
+//  Created by oneweek on 10/15/18.
+//  Copyright © 2018 Harry Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
